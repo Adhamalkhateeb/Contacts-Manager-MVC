a1550acf75bd44a3bf8dd3a2b7fd692226c9a3f8
Adhamalkhateeb <adhamalkhateeb2006@gmail.com>
2026-02-21 15:09:02 +0200

    Remove tracked build artifacts and add gitignore

--- Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e3f64817ddcf82c0fdf56dc7a8e737769cd117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d659157583998912ed04c99ba9fc9b7a9886d8ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
